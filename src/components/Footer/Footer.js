09baf44bf840e4bc8c6dfea368a79022df036345
Asmar <asmarcd@gmail.com>
2020-11-10 11:52:53 -0800

    basic file structure and footer complete

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./style.css";
+
+// This module controls the simple footer at the bottom of the page
+
+function Footer() {
+    return (
+      <footer className="footer">
+        <span>&#169; Chris Asmar 2020</span>
+      </footer>
+    );
+  }
+  
+  export default Footer;
