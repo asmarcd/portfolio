e8d2acbb117a0698a62cf0f22fbca56c25250d82
Chris Asmar <asmarcd@gmail.com>
2022-05-04 14:50:43 -0700

    fixed github link

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -9,8 +9,15 @@ function Project(props) {
                     <h5 className="card-title">{props.name}</h5>
                     <h6 className="card-subtitle mb-2 text-muted">{props.date}</h6>
                     <p className="card-text">{props.description}</p>
-                    <a href={props.activelink} className= "card-link">See it in action</a>
-                    <a href={props.githublink} className="card-link">Github Repo</a>
+                    {props.githublink !== "None" 
+                    ? <a href={props.activelink} className= "card-link">See it in action</a>
+                    : <a href={props.activelink} className= "card-link">Visit Website</a>
+                    }
+                    {props.githublink !== "None" 
+                    ? <a href={props.githublink} className="card-link">Github Repo</a> 
+                    : <a> Private Repo</a>
+                    }
+                    
                 </div>
             </div>
     );
